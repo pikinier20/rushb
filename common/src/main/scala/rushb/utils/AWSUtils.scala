8bb8f1a03fc1ae29788391da14fd6e0a2c29f0fd
Marcin Aman <marcin.aman@gmail.com>
2022-01-09 13:31:09 +0100

    Dzialaj uju

--- common/src/main/scala/rushb/utils/AWSUtils.scala
+++ common/src/main/scala/rushb/utils/AWSUtils.scala
@@ -7,11 +7,11 @@ import com.amazonaws.services.sqs.{AmazonSQS, AmazonSQSClientBuilder}
 object AWSUtils {
   val region: Regions = Regions.EU_WEST_1
 
-  val bucketName: String = "csgo-demos-rushb"
+  def bucketName: String = sys.env.getOrElse("CF_demoBucket", "downloaded-demos-bucket")
   val s3: AmazonS3 = AmazonS3ClientBuilder.standard().withRegion(region).build()
 
-  val crawlerQueueName: String = "https://sqs.eu-west-1.amazonaws.com/743262912284/rushb-queue.fifo"
-  val downloaderQueueName: String = "https://sqs.eu-west-1.amazonaws.com/743262912284/rushb-parser-queue.fifo"
+  def crawlerQueueName: String = sys.env.getOrElse("CF_linksQueue", "https://sqs.eu-west-1.amazonaws.com/743262912284/rushb-queue.fifo")
+  def downloaderQueueName: String = sys.env.getOrElse("CF_downloadedDemosQueue","https://sqs.eu-west-1.amazonaws.com/743262912284/rushb-parser-queue.fifo")
   val sqs: AmazonSQS = AmazonSQSClientBuilder.standard().withRegion(region).build()
 
   def demoLink(id: String) = s"demolink${id}.json"
